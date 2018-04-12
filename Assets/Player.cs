b1aa9e5832dc472b027c3b2172902a1b22d4fc19
Billy Eclipse <Billy Eclipse>
2018-04-12 23:03:27 +0300

    Lecture 276 : Using CrossPlatformManager

---
+++ Assets/Player.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+		Debug.Log(CrossPlatformInputManager.GetAxis("Horizontal"));
+	}
+}
